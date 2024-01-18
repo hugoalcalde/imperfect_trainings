8eed7ca6107102e6a5b62734d0005b7ab0a250e1
Hugo Alcalde <46930484+hugoalcalde@users.noreply.github.com>
2024-01-18 16:56:51 +0100

    Update predict_model.py -> data parallelization

--- imperfect_trainings/predict_model.py
+++ imperfect_trainings/predict_model.py
@@ -1,9 +1,15 @@
-import torch
+from torch import nn 
+import torch 
+from monai.networks.nets import densenet121
+import time 
+
 
 def predict(
     model: torch.nn.Module,
-    dataloader: torch.utils.data.DataLoader
+    dataloader: torch.utils.data.DataLoader, 
+    gpu : int
 ) -> None:
+
     """Run prediction for a given model and dataloader.
     
     Args:
@@ -14,4 +20,23 @@ def predict(
         Tensor of shape [N, d] where N is the number of samples and d is the output dimension of the model
 
     """
-    return torch.cat([model(batch) for batch in dataloader], 0)
+
+    if gpu == 2 : 
+        model = nn.DataParallel(model, device_ids=[0,1])
+    elif gpu == 4 : 
+        model = nn.DataParallel(model, device_ids=[0,1, 2,3])
+    elif gpu == 8 : 
+        model = nn.DataParallel(model, device_ids=[0,1, 2,3, 4, 5, 6, 7])
+
+    return torch.cat([model(batch) for batch, label in dataloader], 0)
+
+model = densenet121(spatial_dims=2, in_channels=1, out_channels=2)
+model.load_state_dict(torch.load("models/baselinetraining/checkpoint.pth"))   
+dataloader = torch.load("data/processed/processed_tensor.pt")["train_loader"]
+for i in [0,2,4,8] : 
+
+    start = time.time()
+    prediction = predict(model, dataloader, i)
+    end = time.time()
+
+    print(f"The inference time using {i} gpus is {end-start}")
